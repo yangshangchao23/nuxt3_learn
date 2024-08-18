0f1f84b03b64d41586fbe073893ef9efa9762bef
yangshangchao <yangshangchao@foresee.com.cn>
2024-08-19 01:09:13 +0800

    badcase

--- app.vue
+++ app.vue
@@ -4,3 +4,8 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script lang="ts" setup>
+const year: number = 2023
+const title: string = `${year} 2023 Nuxt3`
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+console.warn('警告打印输出');
+
+console.log('正常打印输出');
+
+console.error('错误打印输出');
+
+console.info('错误打印输出');
+</script>
