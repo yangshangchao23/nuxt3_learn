37979761a3d30ead1a14b3e46e3a0069a802b298
shawnyoung <990869833@qq.com>
2025-03-06 23:47:19 +0800

    更改打包配置至github page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    baseURL: '/nuxt3_learn/',
+  },
+  ssr: false,
+  nitro: {
+    output: {
+      publicDir: 'docs'
+    }
+  },
+  experimental: {
+    payloadExtraction: false
+  }
 })
